9572fd89b9113617faacdfd9a0c29bcefec5caa4
m-shehata <mido_matrix6000@yahoo.com>
2017-03-18 09:03:07 +0200

    getting list of product details in wish list

--- application/controllers/WishlistController.php
+++ application/controllers/WishlistController.php
@@ -13,7 +13,16 @@ class WishlistController extends Zend_Controller_Action
         $wish_model = new Application_Model_Wishlist();
         $id=1;
         $user_wishlist=$wish_model->userWishlist($id);
+        $prod_model = new Application_Model_Product();
+        $prod_array=array();
         
+        foreach ($user_wishlist as $key => $value) {
+            if ($key == 'productID') {
+                $prod_array[]=$prod_model->productDetails($value);
+            }
+        }
+        
+        $this->view->prodcut_array=$prod_array;
         
         $this->view->user_wishlist=$user_wishlist;
         
